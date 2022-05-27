afcc72c74577d84681d88b9104ececc499b296ec
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-05-27 12:33:01 +0530

    Added

---
+++ Day 28 AddressBook System Problems/AddressBookSystem-3/src/com/bl/addressbook/contact.java
@@ -0,0 +1,14 @@
+package com.bl.addressbook;
+
+public class contact {
+
+	public contact(String fName1, String lName1, int number1, String city1, String state1, int pinCode1) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getfName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
