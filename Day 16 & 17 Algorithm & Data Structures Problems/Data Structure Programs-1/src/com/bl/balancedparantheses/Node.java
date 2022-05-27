83ee4d40985a729877a0c495a56f821beedac905
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-05-27 12:19:49 +0530

    Added

---
+++ Day 16 & 17 Algorithm & Data Structures Problems/Data Structure Programs-1/src/com/bl/balancedparantheses/Node.java
@@ -0,0 +1,13 @@
+package com.bl.balancedparantheses;
+
+public class Node {
+
+	public Node next;
+	public char[] element;
+	public Node next1;
+
+	public Node(String data) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Day 16 & 17 Algorithm & Data Structures Problems/Data Structure Programs-2/src/com/bl/cashcounter/Person.java
@@ -0,0 +1,13 @@
+package com.bl.cashcounter;
+
+public class Person {
+	Person nextPerson;
+	int money;
+	int bankBalance = 1000;
+	public Person( int money) {
+		this.nextPerson = null;
+		this.money = money;
+		CashCounter.noOfCustomers++;
+	}
+
+}	
